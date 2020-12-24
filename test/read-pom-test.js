2c2026d16df5b13dc99c45f73c85b11031a04f27
tai-tran-tan <44283461+tai-tran-tan@users.noreply.github.com>
2020-12-24 18:21:05 +0700

    Update read-pom-test.js

--- test/read-pom-test.js
+++ test/read-pom-test.js
@@ -1,14 +1,5 @@
 const reader = require('../src/read-pom');
 const pomFiles = [
-    'C:\\work\\cob\\cob_portal_template\\pom.xml',
-    'C:\\work\\cob\\cob_portal_style\\pom.xml',
-    'C:\\work\\cob\\cob_portal_kit\\pom.xml',
-    'C:\\work\\cob\\desk_individual_customer1\\pom.xml',
-    'C:\\work\\cob\\individualch\\pom.xml',
-    'C:\\work\\cob\\desk_individual\\pom.xml',
-    'C:\\work\\cob\\cob_basic_component\\pom.xml',
-    'C:\\work\\cob\\cob_portalcustomization\\pom.xml',
-    'C:\\work\\cob\\cob_standard\\pom.xml',
 ];
 
 console.log(reader(pomFiles));
